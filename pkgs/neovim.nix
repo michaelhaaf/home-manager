09ba6715625f311dfa2b402274b4cc6163855726
michaelhaaf <michael.haaf@gmail.com>
2026-01-21 12:26:21 -0500

    update neovim stuff

--- pkgs/neovim.nix
+++ pkgs/neovim.nix
@@ -1,8 +1,59 @@
-{
-  pkgs,
-  ...
-}: {
+{ config, pkgs, lib, ... }:
+
+with lib;
+let
+  build-dependent-pkgs = with pkgs; [
+    acl
+    attr
+    bzip2
+    curl
+    dbus
+    fontconfig
+    freetype
+    gcc
+    glib
+    glibc
+    libcxx
+    libglvnd
+    libsodium
+    libssh
+    libx11
+    libxcb-cursor
+    libxkbcommon
+    libxml2
+    openssl
+    stdenv.cc.cc
+    stdenv.cc.cc.lib
+    systemd
+    util-linux
+    wayland
+    xz
+    zlib
+    zstd
+  ];
+
+  neovim-build-deps = pkgs.buildEnv {
+    name = "neovim-build-deps";
+    paths = build-dependent-pkgs;
+    extraOutputsToInstall = [ "dev" ];
+    pathsToLink = [ "/lib" "/include" ];
+    ignoreCollisions = true;
+  };
+
+  buildEnv = [
+    "CPATH=\${CPATH:+\${CPATH}:}${neovim-build-deps}/include"
+    "CPLUS_INCLUDE_PATH=\${CPLUS_INCLUDE_PATH:+\${CPLUS_INCLUDE_PATH}:}:${neovim-build-deps}/include/c++/v1"
+    "LD_LIBRARY_PATH=\${LD_LIBRARY_PATH:+\${LD_LIBRARY_PATH}:}${neovim-build-deps}/lib"
+    "LIBRARY_PATH=\${LIBRARY_PATH:+\${LIBRARY_PATH}:}${neovim-build-deps}/lib"
+    "NIX_LD_LIBRARY_PATH=\${NIX_LD_LIBRARY_PATH:+\${NIX_LD_LIBRARY_PATH}:}${neovim-build-deps}/lib"
+    "PKG_CONFIG_PATH=\${PKG_CONFIG_PATH:+\${PKG_CONFIG_PATH}:}${neovim-build-deps}/include/pkgconfig"
+  ];
+
+in {
+  home.packages = with pkgs; [ ripgrep ];
+
   programs.neovim = {
+    autowrapRuntimeDeps = true;
     enable = true;
     viAlias = true;
     vimAlias = true;
@@ -10,8 +61,23 @@
     defaultEditor = true;
     withNodeJs = true;
     withPython3 = true;
-    extraLuaPackages = ps: [ps.magick];
-    extraPackages = [ pkgs.clang pkgs.statix pkgs.cargo ];
+    withRuby = true;
+    extraLuaPackages = ls: with ls; [ luarocks ];
+    extraPackages = with pkgs; [
+      doq
+      tree-sitter
+      sqlite
+      cargo
+      clang
+      cmake
+      gcc
+      gnumake
+      go
+      ninja
+      pkg-config
+      statix
+      yarn
+    ];
     plugins = [
       pkgs.vimPlugins.nvim-treesitter.withAllGrammars
       # (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: with plugins; [
@@ -28,6 +94,32 @@
       #   typescript
       #   vim
       # ]))
+    ];
+    extraWrapperArgs = [
+      "--suffix"
+      "CPATH"
+      ":"
+      "${neovim-build-deps}/include"
+      "--suffix"
+      "CPLUS_INCLUDE_PATH"
+      ":"
+      "${neovim-build-deps}/include/c++/v1"
+      "--suffix"
+      "LD_LIBRARY_PATH"
+      ":"
+      "${neovim-build-deps}/lib"
+      "--suffix"
+      "LIBRARY_PATH"
+      ":"
+      "${neovim-build-deps}/lib"
+      "--suffix"
+      "PKG_CONFIG_PATH"
+      ":"
+      "${neovim-build-deps}/include/pkgconfig"
+      "--suffix"
+      "NIX_LD_LIBRARY_PATH"
+      ":"
+      "${neovim-build-deps}/lib"
     ];
   };
 }
